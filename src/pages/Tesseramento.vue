3ed3cf7e0233a4339af3feba6760e6d3bef3aac3
Francesco Brachini <gunner19751@gmail.com>
2022-11-25 16:44:17 +0100

    ...

--- src/pages/Tesseramento.vue
+++ src/pages/Tesseramento.vue
@@ -1,49 +1,42 @@
 <template>
   <div class="flex flex-col min-h-screen overflow-hidden">
-
     <!-- Site header -->
     <Header />
 
     <!-- Page content -->
     <main class="grow">
-
       <!-- Page illustration -->
-      <div class="relative max-w-6xl mx-auto h-0 pointer-events-none" aria-hidden="true">
+      <div
+        class="relative max-w-6xl mx-auto h-0 pointer-events-none"
+        aria-hidden="true"
+      >
         <PageIllustration />
       </div>
 
       <!-- Page sections -->
-       <HeroAboutQ :title="'TESSERAMENTO'"/>
-      <!-- <TimelineQ /> -->
-      <!-- <FeaturesBlocksQ/> -->
-      <!-- <FormSalespersonQ/> -->
-
+      <HeroAboutQ :title="'TESSERAMENTO'" />
+      <FaqsQ />
     </main>
 
     <!-- Site footer -->
     <Footer />
-
   </div>
 </template>
 
 <script>
-import Header from './../partials/Header.vue'
-import PageIllustration from '../partials/PageIllustration.vue'
-import HeroAboutQ from '../partials/becomeSalespersonQ/HeroAboutQ.vue'
-// import TimelineQ from '../partials/becomeSalespersonQ/TimelineQ.vue'
-// import FeaturesBlocksQ from "../partials/becomeSalespersonQ/FeaturesBlocksQ.vue";
-// import FormSalespersonQ from "../partials/becomeSalespersonQ/FormSalespersonQ.vue";
-import Footer from './../partials/Footer.vue'
+import Header from "./../partials/Header.vue";
+import PageIllustration from "../partials/PageIllustration.vue";
+import HeroAboutQ from "../partials/becomeSalespersonQ/HeroAboutQ.vue";
+import FaqsQ from "./../partials/FaqsQ.vue";
+import Footer from "./../partials/Footer.vue";
 
 export default {
-  name: 'becomeSalesperson',
+  name: "becomeSalesperson",
   components: {
-    // FormSalespersonQ,
-    // FeaturesBlocksQ,
     Header,
     PageIllustration,
     HeroAboutQ,
-    // TimelineQ,
+    FaqsQ,
     Footer,
   },
 };
---
+++ src/partials/FaqsQ.vue
@@ -0,0 +1,105 @@
+<template>
+  <section class="pt-28">
+    <div class="max-w-6xl mx-auto px-4 sm:px-6">
+      <div class="py-12 lg:pt-0">
+        <!-- Section header -->
+        <div class="max-w-3xl mx-auto text-center pb-12">
+          <h2 class="h2">6 buoni motivi per tesserarti:</h2>
+        </div>
+
+        <!-- Faqs -->
+        <ul class="grid gap-8 md:grid-cols-2 xl:gap-x-16 lg:gap-y-12">
+          <li class="flex md:justify-start">
+            <div
+              class="inline-flex items-center relative right-3 md:right-5 justify-center py-1 px-3 text-sm font-semibold w-12 h-12 text-green-600 bg-green-200 rounded-full"
+            >
+              1
+            </div>
+            <div>
+              <h4 class="text-xl font-medium mb-2">
+                Consulenze professionali veloci
+              </h4>
+              <p class="text-lg text-gray-400">Gratuite e senza limiti.</p>
+            </div>
+          </li>
+          <li class="flex md:justify-start">
+            <div
+              class="inline-flex items-center relative right-3 md:right-5 justify-center py-1 px-3 text-sm font-semibold w-12 h-12 text-green-600 bg-green-200 rounded-full"
+            >
+              4
+            </div>
+            <div>
+              <h4 class="text-xl font-medium mb-2">
+                Pratiche P.A. in tempo reale
+              </h4>
+              <p class="text-lg text-gray-400">
+                Accesso ai servizi della P.A. in tempo reale.
+              </p>
+            </div>
+          </li>
+          <li class="flex md:justify-start">
+            <div
+              class="inline-flex items-center relative right-3 md:right-5 justify-center py-1 px-3 text-sm font-semibold w-12 h-12 text-green-600 bg-green-200 rounded-full"
+            >
+              2
+            </div>
+            <div>
+              <h4 class="text-xl font-medium mb-2">Risoluzioni debiti</h4>
+              <p class="text-lg text-gray-400">
+                Sofferenze bancarie, debiti fiscali, fornitori e
+                sovraindebitamento.
+              </p>
+            </div>
+          </li>
+          <li class="flex md:justify-start">
+            <div
+              class="inline-flex items-center relative md:right-5 justify-center py-1 px-3 text-sm font-semibold w-12 h-12 text-green-600 bg-green-200 rounded-full"
+            >
+              5
+            </div>
+            <div>
+              <h4 class="text-xl font-medium mb-2">
+                Aggiornamenti personalizzati
+              </h4>
+              <p class="text-lg text-gray-400">
+                Nuove normative nazionali, regionali, provinciali e comunali.
+              </p>
+            </div>
+          </li>
+          <li class="flex md:justify-start">
+            <div
+              class="inline-flex items-center relative right-3 md:right-5 justify-center py-1 px-3 text-sm font-semibold w-12 h-12 text-green-600 bg-green-200 rounded-full"
+            >
+              3
+            </div>
+            <div>
+              <h4 class="text-xl font-medium mb-2">Reale accesso al credito</h4>
+              <p class="text-lg text-gray-400">
+                Bandi, fondi del PNRR, finanziamenti, crowdfunding.
+              </p>
+            </div>
+          </li>
+          <li class="flex md:justify-start">
+            <div
+              class="inline-flex items-center relative right-3 md:right-5 justify-center py-1 px-3 text-sm font-semibold w-12 h-12 text-green-600 bg-green-200 rounded-full"
+            >
+              6
+            </div>
+            <div>
+              <h4 class="text-xl font-medium mb-2">Prezzi scontati</h4>
+              <p class="text-lg text-gray-400">
+                - 30% di sconto reale per i tesserati.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FaqsQ",
+};
+</script>
