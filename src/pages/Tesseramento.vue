7eecf8b76c1c18bdb54c931d9ddf5e07bf06f7aa
Francesco Brachini <gunner19751@gmail.com>
2022-11-25 16:54:31 +0100

    ...

--- src/pages/Tesseramento.vue
+++ src/pages/Tesseramento.vue
@@ -16,6 +16,27 @@
       <!-- Page sections -->
       <HeroAboutQ :title="'TESSERAMENTO'" />
       <FaqsQ />
+      <!-- Chi può tesserarsi -->
+      <div class="text-center mt-10 mb-10" data-aos-id-target>
+        <div
+          class="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+        >
+          <div class="mx-auto">
+            <div
+              class="mt-6"
+              data-aos="fade-left"
+              data-aos-delay="200"
+              data-aos-anchor="[data-aos-id-target]"
+            >
+              <h4 class="h4 mb-2">Chi può tesserarsi?</h4>
+              <p class="text-lg text-gray-400 m-6">
+                Professionisti, micro imprese, aziende e privati con intenzione
+                di aprire un'attività.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
 
     <!-- Site footer -->
