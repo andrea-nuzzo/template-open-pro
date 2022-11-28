61ca80530361484bda8a68c8744cd5e1ec7ba28c
Francesco Brachini <gunner19751@gmail.com>
2022-11-28 12:42:05 +0100

    ...

---
+++ src/pages/ChiSiamo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="flex flex-col min-h-screen overflow-hidden">
+    <!-- Site header -->
+    <Header />
+
+    <!-- Page content -->
+    <main class="grow">
+      <!-- Page illustration -->
+      <div
+        class="relative max-w-6xl mx-auto h-0 pointer-events-none"
+        aria-hidden="true"
+      >
+        <PageIllustration />
+      </div>
+
+      <!-- Page sections -->
+      <HeroAboutQ :title="'CHI SIAMO'" />
+      <!-- Hero content -->
+      <div class="relative pt-32 pb-12 md:pt-40 md:pb-20">
+        <!-- Content -->
+        <div class="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+          <h1 class="h1 mb-4 text-left" data-aos="fade-up">Facciamo le presentazioni.</h1>
+          <p
+            class="text-xl text-gray-400 py-7"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            Siamo VeMa web srl, una startup innovativa che lavora in remote
+            working ad un grande progetto. Lo facciamo con professionalità e
+            metodo, come se lavorassimo insieme da sempre.
+          </p>
+          <h1 class="h1 mb-4 text-right" data-aos="fade-up">Mission.</h1>
+          <p
+            class="text-xl text-gray-400 py-7"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            Stiamo realizzando la prima Rete di sviluppo economico nazionale.
+            Questo significa prendersi la responsabilità di ridare davvero nuova
+            forza al tessuto economico su cui si fonda il nostro Paese, le
+            economie locali e le PMI.
+          </p>
+        </div>
+      </div>
+    </main>
+
+    <!-- Site footer -->
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./../partials/Header.vue";
+import PageIllustration from "../partials/PageIllustration.vue";
+import HeroAboutQ from "../partials/becomeSalespersonQ/HeroAboutQ.vue";
+import FeaturesZigzagQ from "../partials/FeaturesZigzagQ.vue";
+import Footer from "./../partials/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    PageIllustration,
+    HeroAboutQ,
+    Footer,
+  },
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,123 +1,130 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from './pages/Home.vue'
-import Features from './pages/Features.vue'
-import Pricing from './pages/Pricing.vue'
-import Blog from './pages/Blog.vue'
-import BlogPost from './pages/BlogPost.vue'
-import About from './pages/About.vue'
-import Contact from './pages/Contact.vue'
-import Help from './pages/Help.vue'
-import SignIn from './pages/SignIn.vue'
-import SignUp from './pages/SignUp.vue'
-import ResetPassword from './pages/ResetPassword.vue'
-import PageNotFound from './pages/PageNotFound.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./pages/Home.vue";
+import Features from "./pages/Features.vue";
+import Pricing from "./pages/Pricing.vue";
+import Blog from "./pages/Blog.vue";
+import BlogPost from "./pages/BlogPost.vue";
+import About from "./pages/About.vue";
+import ChiSiamo from "./pages/ChiSiamo.vue";
+import Contact from "./pages/Contact.vue";
+import Help from "./pages/Help.vue";
+import SignIn from "./pages/SignIn.vue";
+import SignUp from "./pages/SignUp.vue";
+import ResetPassword from "./pages/ResetPassword.vue";
+import PageNotFound from "./pages/PageNotFound.vue";
 import becomeSalesperson from "./pages/BecomeSalespersonQ.vue";
-import Tesseramento from './pages/Tesseramento.vue';
+import Tesseramento from "./pages/Tesseramento.vue";
 import subscription from "./pages/SubscriptionQ.vue";
 import Servizi from "./pages/ServiziQ.vue";
 import Servizio from "./pages/ServizioQ.vue";
 import data from "./data/servizi.json";
 
-const routerHistory = createWebHistory()
+const routerHistory = createWebHistory();
 
 const router = createRouter({
   scrollBehavior(to) {
     if (to.hash) {
-      window.scroll({ top: 0 })
+      window.scroll({ top: 0 });
     } else {
-      document.querySelector('html').style.scrollBehavior = 'auto'
-      window.scroll({ top: 0 })
-      document.querySelector('html').style.scrollBehavior = ''
+      document.querySelector("html").style.scrollBehavior = "auto";
+      window.scroll({ top: 0 });
+      document.querySelector("html").style.scrollBehavior = "";
     }
   },
   history: routerHistory,
   routes: [
     {
-      path: '/',
-      component: Home
+      path: "/",
+      component: Home,
     },
     {
-      path: '/features',
-      component: Features
+      path: "/features",
+      component: Features,
     },
     {
-      path: '/pricing',
-      component: Pricing
+      path: "/pricing",
+      component: Pricing,
     },
     {
-      path: '/blog',
-      component: Blog
+      path: "/blog",
+      component: Blog,
     },
     {
-      path: '/blog-post',
-      component: BlogPost
+      path: "/blog-post",
+      component: BlogPost,
     },
     {
-      path: '/about',
-      component: About
+      path: "/about",
+      component: About,
     },
     {
-      path: '/contact',
-      component: Contact
+      path: "/chi-siamo",
+      component: ChiSiamo,
     },
     {
-      path: '/help',
-      component: Help
+      path: "/contact",
+      component: Contact,
     },
     {
-      path: '/diventa-rappresentante/:token?',
+      path: "/help",
+      component: Help,
+    },
+    {
+      path: "/diventa-rappresentante/:token?",
       component: becomeSalesperson,
     },
     {
-      path: '/subscription',
+      path: "/subscription",
       component: subscription,
     },
     {
-      path: '/servizi-per-te',
+      path: "/servizi-per-te",
       component: Servizi,
     },
     {
-      path: '/:slug',
-      name:'Servizio',
+      path: "/:slug",
+      name: "Servizio",
       component: Servizio,
 
-      beforeEnter (to, from, next) {
-        const exists = data.servizi.find(servizi => servizi.slug === to.params.slug)
+      beforeEnter(to, from, next) {
+        const exists = data.servizi.find(
+          (servizi) => servizi.slug === to.params.slug
+        );
 
         if (exists) {
-          return next()
+          return next();
         } else {
           next({
-            name: 'PageNotFound',
-            params: {pathMatch: to.path.substring(1).split('/')},
+            name: "PageNotFound",
+            params: { pathMatch: to.path.substring(1).split("/") },
             query: to.query,
             hash: to.hash,
-          })
+          });
         }
-      }
+      },
     },
     {
-      path: '/signin',
-      component: SignIn
+      path: "/signin",
+      component: SignIn,
     },
     {
-      path: '/signup',
-      component: SignUp
+      path: "/signup",
+      component: SignUp,
     },
     {
-      path: '/reset-password',
-      component: ResetPassword
+      path: "/reset-password",
+      component: ResetPassword,
     },
     {
-      path: '/tesseramento',
-      component: Tesseramento
+      path: "/tesseramento",
+      component: Tesseramento,
     },
     {
-      path: '/:NotFound(.*)*',
-      name:'PageNotFound',
-      component: PageNotFound
-    }
-  ]
-})
+      path: "/:NotFound(.*)*",
+      name: "PageNotFound",
+      component: PageNotFound,
+    },
+  ],
+});
 
-export default router
+export default router;
