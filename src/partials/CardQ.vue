7f4b702e44586ebba7731932c3df7c38290e8d10
Francesco Brachini <gunner19751@gmail.com>
2022-11-29 21:33:37 +0100

    ...

--- src/partials/CardQ.vue
+++ src/partials/CardQ.vue
@@ -1,34 +1,25 @@
 <template>
-  <transition
-    enter-active-class="transition ease-in-out duration-500 transform order-first"
-    enter-from-class="opacity-0 scale-98"
-    enter-to-class="opacity-100 scale-100"
-    leave-active-class="transition ease-out duration-300 transform absolute"
-    leave-from-class="opacity-100 scale-100"
-    leave-to-class="opacity-0 scale-98"
-  >
-    <div class="w-full">
-      <article class="relative max-w-md mx-auto md:max-w-none">
-        <figure class="md:absolute md:inset-y-0 md:right-0 md:w-1/2">
-          <img
-            class="w-full h-full object-cover"
-            :src="logo"
-            width="516"
-            height="387"
-            alt="logo"
-          />
-        </figure>
-        <div
-          class="relative bg-gray-800 py-8 md:py-16 px-6 md:pr-16 md:max-w-lg lg:max-w-xl"
-        >
-          <h4 class="h4 mb-2">{{ title }}</h4>
-          <p class="text-lg text-gray-400">
-            {{ content }}
-          </p>
-        </div>
-      </article>
-    </div>
-  </transition>
+  <div class="w-full" data-aos="fade-left" data-aos-delay="300">
+    <article class="relative max-w-md mx-auto md:max-w-none">
+      <figure class="md:absolute md:inset-y-0 md:right-0 md:w-1/2">
+        <img
+          class="w-full h-full object-cover"
+          :src="logo"
+          width="516"
+          height="387"
+          alt="logo"
+        />
+      </figure>
+      <div
+        class="relative bg-gray-800 py-8 md:py-16 px-6 md:pr-16 md:max-w-lg lg:max-w-xl"
+      >
+        <h4 class="h4 mb-2">{{ title }}</h4>
+        <p class="text-lg text-gray-400">
+          {{ content }}
+        </p>
+      </div>
+    </article>
+  </div>
 </template>
 <script>
 export default {
