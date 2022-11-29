b0ce41ec562e61f01ec056b4662d8342df2cc6e6
Francesco Brachini <gunner19751@gmail.com>
2022-11-29 12:22:17 +0100

    ...

--- src/pages/FacciamoReteQ.vue
+++ src/pages/FacciamoReteQ.vue
@@ -38,11 +38,11 @@
       <div class="relative pb-12 md:pb-20">
         <!-- Content -->
         <div class="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-          <section class="flex justify-start">
+          <section class="flex justify-start pb-10 md:pb-14">
             <div
-            data-aos="fade-right"
+              data-aos="fade-right"
               data-aos-delay="200"
-              class="inline-flex items-center relative right-2 md:right-3 justify-center py-3 px-4 text-sm font-semibold w-8 h-8 text-white bg-black rounded-full"
+              class="inline-flex items-center relative md:right-2 lg:right-7 justify-center py-3 px-4 text-sm font-semibold w-8 h-8 text-white bg-black rounded-full"
             >
               1
             </div>
@@ -64,7 +64,7 @@
               </p>
             </div>
           </section>
-          <section class="flex justify-end">
+          <section class="flex justify-end pb-10 md:pb-14">
             <div
               class="flex flex-col items-end"
               data-aos="fade-up"
@@ -74,20 +74,44 @@
                 Nasce la soluzione perfetta: Quisviluppo.com
               </h4>
               <p class="text-right">
-                Come riportare le persone a comprare nei negozi. Rendendo le
-                economie locali molto più competitive e capaci di superare i
-                benefici ottenuti acquistando dai colossi delle vendite online.
-                Abbiamo creato strumenti web ad hoc e stravolto il modo di
-                vendere, spedire, gestire e fare promozione. Diamo, anche,
-                grande importanza alle competenze professionali.
+                Sette piattaforme web sono la soluzione completa per ridare
+                potere alle economie locali. Sono la base della nostra Rete di
+                sviluppo economico nazionale. Ogni singolo negozio, azienda,
+                artigiano e professionista sono le realtà vere che faranno da
+                carburante alla Rivoluzione. Perché di questo si tratta.
               </p>
             </div>
             <div
-            data-aos="fade-right"
+              data-aos="fade-left"
               data-aos-delay="200"
-              class="inline-flex items-center relative left-2 md:left-3 justify-center py-3 px-4 text-sm font-semibold w-8 h-8 text-white bg-black rounded-full"
+              class="inline-flex items-center relative md:left-2 lg:left-7 justify-center py-3 px-4 text-sm font-semibold w-8 h-8 text-white bg-black rounded-full"
             >
-              1
+              2
+            </div>
+          </section>
+          <section class="flex justify-start">
+            <div
+              data-aos="fade-right"
+              data-aos-delay="200"
+              class="inline-flex items-center relative md:right-2 lg:right-7 justify-center py-3 px-4 text-sm font-semibold w-8 h-8 text-white bg-black rounded-full"
+            >
+              3
+            </div>
+            <div
+              class="flex flex-col items-start"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
+              <h4 class="h4 mb-5">
+                Rete di sviluppo economico nazionale. Uniamoci e facciamo rete
+              </h4>
+              <p class="text-left">
+                A cosa serve la Rete? A valorizzare la nostra cultura, i valori
+                sani dell’impegno, dell’innovazione e della voglia di
+                migliorarsi. Noi di Quisviluppo.com sappiamo come fare, sappiamo
+                come vincere. Nei nostri canali social usiamo l’#eroepernoi.
+                Questa è la nostra arma vincente, sei Tu. Siamo tutti Noi.
+              </p>
             </div>
           </section>
         </div>
